561a48b3e1d3d728f8337802fa8e9f2d5358eecd
Abdelaziz Tantawy <AbdelazizTantawy1@gmail.com>
2025-09-19 21:41:23 +0300

    Class1.cs

--- ConsoleApp4/Class1.cs
+++ ConsoleApp4/Class1.cs
@@ -120,22 +120,6 @@ namespace ConsoleApp4
             #region Q15
             // Answer : d) 7 7
             #endregion
-
-            #region Q11
-            //Console.Write("Enter Day: ");
-            //string d = Console.ReadLine();
-            //Console.Write("Enter Month: ");
-            //string m = Console.ReadLine();
-            //Console.Write("Enter Year: ");
-            //string y = Console.ReadLine();
-            //Console.WriteLine($"Today's Date : {d} , {m} , {y}");
-            //Console.WriteLine($"Today's Date : {d} / {m} / {y}");
-            //Console.WriteLine($"Today's Date : {d} - {m} - {y}");
-            #endregion
-
-            #region Q12
-            // Answer : a) 14/06/2024
-            #endregion
         }
     }
 }
